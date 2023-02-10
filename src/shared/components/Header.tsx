34ad0a93448b871e6cbb4bb3b376bb35008c88c4
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-02-10 09:46:31 +0100

    added img and gradient button

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../images/logo.png";
 
 export const Header = () => {
   return (
     <div className="header">
       <h1>MovieDB</h1>
+      <img src={logo} alt="logo app" width={50} height={50}/>
       {/* <img src="images/logo.png" alt="logo app" /> */}
       <div>
         <input className= "search" type= "text"/>

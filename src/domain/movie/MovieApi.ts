f1daa7a1d7fbc8e43951f23245aaddfb086154f9
nedys55 <denysyuksek@gmail.com>
2023-02-10 16:01:09 +0100

    fix: apikey and .gitignore

--- src/domain/movie/MovieApi.ts
+++ src/domain/movie/MovieApi.ts
@@ -2,14 +2,16 @@ import React from 'react';
 import { MovieType, MovieDetailType } from './MovieModels';
 import axios from 'axios';
 
+const apiKey = process.env.API_KEY
+
 export const getMoviesAll  = async () => {
-    const allMoviesURL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=fr-FR`
+    const allMoviesURL = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr-FR`
 
     return axios.get<{results: MovieType[]}>(allMoviesURL).then((response) => response.data.results)
 }
 
 export const getMovieDetail =  async (id: number) => {
-    const MovieURL = `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=fr-FR`
+    const MovieURL = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=fr-FR`
 
     return axios.get<{results: MovieDetailType[]}>(MovieURL).then((response) => response.data.results)
 }

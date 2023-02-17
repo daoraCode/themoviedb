0576cdc394598ce486165c5065d40eef222bf32e
daoraCode <severin.mboukou@gmail.com>
2023-02-17 12:21:14 +0100

    sidebar setup

--- src/domain/category/CategoryApi.ts
+++ src/domain/category/CategoryApi.ts
@@ -2,14 +2,10 @@
 import axios from "axios";
 import { CategoryType } from "./CategoryType";
 
-const apiKey = process.env.API_KEY
-<<<<<<< HEAD
-=======
-
->>>>>>> 849fa9c (fix: resolving conflicts)
+const apiKey = process.env.REACT_APP_API_KEY
 // async function
 export const getMovieCategories = async () => {
     const apiUrlGenres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=fr-FR`
-    return axios.get<{results: CategoryType[]}>(apiUrlGenres)
-                .then((response) => response.data.results)
+    return axios.get<{genres: CategoryType[]}>(apiUrlGenres)
+                .then((response) => response.data.genres)
 }
